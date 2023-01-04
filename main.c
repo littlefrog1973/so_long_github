936aecdf827fa75a040a8329efb06c0b630e6779
Sukit Deeyiengyang <kitdee@hotmail.com>
2023-01-04 16:14:13 +0700

    complete simple check map without floodfill

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sdeeyien <sukitd@gmail.com>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/17 11:12:45 by sdeeyien          #+#    #+#             */
-/*   Updated: 2022/12/21 16:45:25 by sdeeyien         ###   ########.fr       */
+/*   Updated: 2023/01/04 15:39:01 by sdeeyien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,116 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include "libft/libft.h"
+
+void err_exit(char **map, t_list **map_list_ptr, int err_no)
+{
+	ft_lstclear(map_list_ptr, free);
+	free(map);
+	if (err_no == 0)
+		return ;
+	if (err_no == 1)
+		ft_printf("Error\nMap is composed of wrong letter.\n");
+	if (err_no == 2)
+		ft_printf("Error\nMap has multiple entrance or exit.\n");
+	if (err_no == 3)
+		ft_printf("Error\nMap is missing of entrance or exit.\n");
+	if (err_no == 4)
+		ft_printf("Error\nMap is not rectangular. or closed.\n");
+	if (err_no == 5)
+		ft_printf("Error\nMap has no collectible.\n");
+	exit(1);
+}
+
+int count_chr(const char *row, const char letter)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (row[i] != '\0')
+	{
+		count += (row[i] == letter);
+		i++;
+	}
+	return (count);
+}
+
+int chk_rectan(char **map)
+{
+	int	i;
+	int	diff;
+	int	row_length;
+
+	i = 0;
+	diff = 0;
+	row_length = (int) ft_strlen(map[i]);
+	while (map[i])
+	{
+		if (row_length - (int) ft_strlen(map[i]))
+			diff = 1;
+		i++;
+	}
+	return (!diff);
+}
+
+int chk_wall(char **map)
+{
+	int	is_closed;
+	int	i;
+
+	i = 0;
+	is_closed = 1;
+	while (map[i])
+	{
+		is_closed &= (map[i][0] == '1' && map[i][(int) ft_strlen(map[i]) - 1] == '1');
+		i++;
+	}
+	is_closed &= count_chr(map[i - 1], '1') && (int) ft_strlen(map[i - 1]);
+	is_closed &= count_chr(map[0], '1') && (int) ft_strlen(map[0]);
+	return (is_closed);
+}
+
+void chk_map(char **map, t_list **map_list_ptr)
+{
+	int	i;
+	int	j;
+	int	no_exit;
+	int	no_collect;
+	int	no_start;
+
+	no_exit = 0;
+	no_collect = 0;
+	no_start = 0;
+	i = 0;
+	if (chk_rectan(map) && chk_wall(map))
+	{
+		while (map[i])
+		{
+			j = 0;
+			while (map[i][j])
+			{
+				if (!ft_strchr("01CEP", map[i][j]))
+					err_exit(map, map_list_ptr, 1);
+				no_exit += ('E' == map[i][j]);
+				no_collect += ('C' == map[i][j]);
+				no_start += ('P' == map[i][j]);
+				j++;
+			}
+			i++;
+		}
+		if (no_exit == 1 && no_start == 1 && no_collect >= 1)
+			return ;
+		else if (no_exit >= 2 || no_start >= 2)
+			err_exit(map, map_list_ptr, 2);
+		else if (no_collect == 0)
+			err_exit(map, map_list_ptr, 5);
+		else
+			err_exit(map, map_list_ptr, 3);
+	}
+	else
+		err_exit(map, map_list_ptr, 4);
+}
 
 t_list **read_file(int argc, char *argv[], t_list **map_list_ptr)
 {
@@ -28,11 +138,23 @@ t_list **read_file(int argc, char *argv[], t_list **map_list_ptr)
 		{
 			map_list = ft_lstnew(get_next_line(fd));
 			*map_list_ptr = map_list;
+			while (ft_lstlast(*map_list_ptr)->content)
+			{
+				ft_lstadd_back(map_list_ptr, ft_lstnew(get_next_line(fd)));
+			}
+/*			if (map_list->content == NULL)
+				exit (1);
 			while (1)
 			{
 				ft_lstadd_back(map_list_ptr, ft_lstnew(get_next_line(fd)));
 				if (!ft_lstlast(*map_list_ptr)->content)
 					break;
+			}
+*/
+			if (!map_list->content)
+			{
+				free(map_list);
+				exit (1);
 			}
 			fd = close(fd);
 			return (map_list_ptr);
@@ -66,7 +188,6 @@ char **parse_map(int argc, char *argv[], t_list **map_list_ptr)
 		map_runner = map_runner->next;
 		i++;
 	}
-	ft_printf("In parse_map i=%d\n", i);
 	map_array[i] = NULL;
 	return (map_array);
 }
@@ -74,18 +195,18 @@ char **parse_map(int argc, char *argv[], t_list **map_list_ptr)
 int	main(int argc, char *argv[])
 {
 	char	**map;
-	t_list	**map_list_ptr;
-	t_list	*dummy;
+	t_list	*map_list_ptr;
 	int	i;
 
 	i = 0;
-	map_list_ptr = &dummy;
-	map = parse_map(argc, argv, map_list_ptr);
+	map = parse_map(argc, argv, &map_list_ptr);
+	chk_map(map, &map_list_ptr);
+
 	while (map[i])
 	{
 		ft_printf("parsed map[%d] = %s\n", i, map[i]);
 		i++;
 	}
-	ft_printf("test ft_strlen = %d\n", (int) ft_strlen("abc"));
+	err_exit(map, &map_list_ptr, 0);
 	return (0);
 }
